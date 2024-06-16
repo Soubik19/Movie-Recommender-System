843b2034f0ca381e50542da6986b52ee24a18a75
ImSoubik <166213028+Soubik19@users.noreply.github.com>
2024-06-16 22:55:30 +0530

    Update app.py

--- movie-recommender-system/app.py
+++ movie-recommender-system/app.py
@@ -1,12 +1,43 @@
 import streamlit as st
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import os
 import pickle
 import requests
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('movie-recommender-system/movie_list.pkl', 'rb'))
 
+def fetch_poster(movie_id):
+    api_key = os.getenv('API_KEY')
+
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.getenv('AUTH_TOKEN')}"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    poster_path = data.get('poster_path', 'No poster available')
+    full_path = f"https://image.tmdb.org/t/p/w500{poster_path}"
+    return full_path
+
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names, recommended_movie_posters
+
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movie_list.pkl', 'rb'))
 
 cv = CountVectorizer(max_features=5000, stop_words='english')
 vector = cv.fit_transform(movies['tags']).toarray()
@@ -18,19 +49,22 @@ selected_movie = st.selectbox(
     movie_list
 )
 
-
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-
-    return recommended_movie_names
-
-
 if st.button('Show Recommendation'):
-    recommended_movie_names = recommend(selected_movie)
-    for movie in recommended_movie_names:
-        st.write(movie)
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
